7dda0dcecd1848e8a0ba86a84d66665810f7d5a1
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2023-12-01 19:07:30 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardGenerator(numeros, simbolos);
 };
+function cardGenerator(numeros, simbolos) {
+  let numrandom = numeros[Math.floor(Math.random() * numeros.length)];
+  let simbolosrandom = simbolos[Math.floor(Math.random() * simbolos.length)];
+
+  let header = (document.querySelector("#simbolo1").innerHTML = simbolosrandom);
+  let body = (document.querySelector("#numeros1").innerHTML = numrandom);
+  let footer = (document.querySelector("#simbolo2").innerHTML = simbolosrandom);
+}
+
+let numeros = ["1", "2", "3", "4", "5", "6", "7"];
+let simbolos = ["♠", "♣", "♥", "♦"];
